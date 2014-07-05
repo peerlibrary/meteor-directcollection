8b690870a0b6b4a0e5277b51f0232f9d067f7431
Mitar <mitar.git@tnode.com>
2014-07-04 23:16:57 -0700

    Initial version.

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  summary: "Blocking MongoDB API for direct/native access to collections"
+});
+
+Package.on_use(function (api) {
+  api.use(['coffeescript', 'underscore', 'random', 'blocking', 'ejson'], 'server');
+
+  api.export('DirectCollection');
+
+  api.add_files([
+    'direct.coffee'
+  ], 'server');
+});
+
+Package.on_test(function (api) {
+  api.use(['directcollection', 'tinytest', 'test-helpers', 'coffeescript', 'underscore'], 'server');
+
+  api.add_files([
+    'tests.coffee'
+  ], 'server');
+});
