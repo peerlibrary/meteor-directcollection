4717f2787de9c76dd7b1a131b039c6f3a9774bb7
Mitar <mitar.git@tnode.com>
2019-09-19 19:04:57 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'peerlibrary:directcollection',
   summary: "Blocking MongoDB API for direct/native access to collections",
-  version: '0.8.0',
+  version: '0.9.0',
   git: 'https://github.com/peerlibrary/meteor-directcollection.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.0.3_3',
+    'coffeescript@2.4.1',
     'ecmascript',
     'underscore',
     'random',
@@ -20,7 +20,7 @@ Package.on_use(function (api) {
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:blocking@0.5.2'
+    'peerlibrary:blocking@0.6.0'
   ], 'server');
 
   api.export('DirectCollection', 'server');
@@ -29,13 +29,13 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
     'tinytest',
     'test-helpers',
-    'coffeescript',
+    'coffeescript@2.4.1',
     'underscore',
     'application-configuration'
   ], 'server');
